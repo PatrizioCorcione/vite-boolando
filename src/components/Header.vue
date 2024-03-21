75794dcb2358f580072d08b036e4cb5edad23744
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-21 16:31:08 +0100

    [add] header fontawesome problem in import

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,70 @@
 <script>
 
+
+
 </script>
 
 <template>
 
-  <div>
-    heloooooooooo
+  <div class="header">
+    <div class="container flex-top">
+      <nav class="nav-text">
+        <ul class="flex-ul-txt">
+          <li><a class="white" href="">Donna</a></li>
+          <li><a class="white" href="">Uomo</a></li>
+          <li><a class="white" href="">Bambini</a></li>
+        </ul>
+      </nav>
+      <div class="logo">
+        <img src="../assets/img/boolean-logo.png" alt="Boolando">
+      </div>
+      <nav class="nav-icon">
+        <ul class="flex-ul">
+          <li><a href=""> <i class="fa-regular fa-user"></i> </a></li>
+          <li><a href=""><i class="fa-regular fa-heart"></i></a></li>
+          <li><a href=""><i class="fa-solid fa-bag-shopping"></i></a></li>
+        </ul>
+      </nav>
+    </div>
+  
   </div>
   
 </template>
 
-<style lang="scss">
+
+
+<style lang="scss" scoped>
+
+@import '../assets/scss/partials/general';
+@import '../assets/scss/partials/variables'; 
+
+  .header{
+    height: 60px;
+    top: 0;
+    z-index: 999;
+    position: fixed;
+    width: 100%;
+    background-color: $headerBack;
+    .container{
+      margin-bottom: 50px;
+      height: 60px;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      .nav-text{
+      width: 40%;
+      .flex-ul-txt{
+        width: 40%;
+        display: flex;
+        justify-content: space-between;
+    }
+    }
+    
+    img{
+      width: 20%;
+    }
+  }
+
+}
 
 </style>
