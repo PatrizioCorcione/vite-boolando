37830bf207a5b1cff3305e5902e8193109a4f0c4
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-03-21 14:17:43 +0100

    [START]

---
+++ README.md
@@ -0,0 +1,7 @@
+# Boolando vite
+
+## SVILUPPO
+
+- Scafolding
+- crerare e linkare i componenti header,main.footer
+-
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <div>
+   viva la fica
+  </div>
+  
+</template>
+
+<style >
+
+</style>
